4c2c63b617488e5aa0a05864970e4f2256f48dd2
poojachamundi123 <poojachamundi1@gmail.com>
2026-05-22 20:48:19 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,34 @@
+# MODEL FILE
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load dataset
+df = pd.read_csv("student_performance.csv")
+
+print(df.head())
+
+# Handle missing values
+df = df.fillna(df.select_dtypes(include=[np.number]).mean())
+
+# Features & target
+X = df.drop("final_score", axis=1)
+y = df["final_score"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
